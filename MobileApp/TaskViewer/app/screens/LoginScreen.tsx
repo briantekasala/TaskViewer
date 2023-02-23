e36c76b4676ac132069a4e5dcdc1880cee8eca5a
Your Name~ <you@example.com>
2023-02-23 11:44:19 +0100

    setuo mobile app

---
+++ MobileApp/TaskViewer/app/screens/LoginScreen.tsx
@@ -0,0 +1,11 @@
+import { Card } from "@mui/material";
+import * as React from "react";
+import { Text, View } from "react-native";
+
+export const LoginScreen = () => {
+  return (
+    <View>
+      <Text>login</Text>
+    </View>
+  );
+};
